f0afd0987acb2f28908f9b4c8b66c32259212e11
Fredrik Jonsson <fredrik.siljeblad@gmail.com>
2022-10-03 14:09:37 +0200

    Pretty much copied Jan's LoginForm to RegisForm...

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -14,7 +14,7 @@ fields.forEach(field=>fieldsState[field.id]='');
 
 function LoginForm({logo}) {
   const [loginState,setLoginState]=useState(fieldsState);
-  const [loggedUser, setLoggedUser] = useState({});
+  //const [loggedUser, setLoggedUser] = useState({});
 
     const handleChange=(e)=>{
       console.log(e.target.id, e.target.value)
@@ -37,6 +37,7 @@ function LoginForm({logo}) {
         "teams": [],
         "invitations": []
       }
+
       const logInUser = async () => {
         let response = await fetch('/api/user/login', {
           method: 'POST',
@@ -44,21 +45,27 @@ function LoginForm({logo}) {
             'Content-Type': 'application/json'},
           body: JSON.stringify(user)
         });
-        console.log(response)
+
         let result = await response.json();
-        console.log(JSON.parse(result))
+
         //setLoggedUser(result);
 
         if (result.status === 200) {
-          onsole.log('User logged in successfully');
+          console.log('User logged in successfully');
+          //Set localStorage for user id
+          localStorage.setItem('active-user-id', logInUser.userId);
           return <Navigate to="/layout" />
-          c
         }
         console.log(result);
       }
-      logInUser(); 
-      //Set cookie for user id
+
+      logInUser(user).then((data) => {
+        console.log("Success");
+        console.log(data);
+      });
+
     }
+
   return (
     <div>
         <form className='max-w-[400px] w-full mb-48 ml-36 bg-white p-4' onSubmit={handleSubmit}>
