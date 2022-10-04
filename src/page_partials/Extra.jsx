028183ab3cdbaab2f59bb34e440099a547a0427b
Jan Beszterda <jan.beszterda@gmail.com>
2022-10-04 22:31:20 +0200

    Fixed missing key errors for board-column and column-card. Corrected link in ProfilePage. Fixed column styling

--- src/page_partials/Extra.jsx
+++ src/page_partials/Extra.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Extra() {
   return (
     <div>
-        <a class="flex items-center px-4 py-2 mt-2 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-                
-               
-                <span class="mx-4 font-medium">Notes</span>
-            </a>
+      <a
+        className="flex items-center px-4 py-2 mt-2 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700"
+        href="#"
+      >
+        <span className="mx-4 font-medium">Notes</span>
+      </a>
 
-            <a class="flex items-center px-4 py-2 mt-3 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-               
+      <a
+        className="flex items-center px-4 py-2 mt-3 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700"
+        href="#"
+      >
+        <span className="mx-4 font-medium">Friends</span>
+      </a>
 
-                <span class="mx-4 font-medium">Friends</span>
-            </a>
-
-            <a class="flex items-center px-4 py-2 mt-3 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-               
-
-                <span class="mx-4 font-medium">Settings</span>
-            </a>
+      <a
+        className="flex items-center px-4 py-2 mt-3 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700"
+        href="#"
+      >
+        <span className="mx-4 font-medium">Settings</span>
+      </a>
     </div>
-  )
+  );
 }
 
-export default Extra
+export default Extra;
