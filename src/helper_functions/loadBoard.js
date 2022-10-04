5b084e61b1ede07be16589c262c033ab51ba34b6
Jan Beszterda <jan.beszterda@gmail.com>
2022-10-04 17:11:48 +0200

    added missing curly bracket

--- src/helper_functions/loadBoard.js
+++ src/helper_functions/loadBoard.js
@@ -1,4 +1,4 @@
-export const loadBoard = async (boardId) => 
+export const loadBoard = async (boardId) => {
   const response = await fetch("/api/board/" + boardId);
   const data = await response.json();
   return data;
