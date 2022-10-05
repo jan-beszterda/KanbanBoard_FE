1a96e55cb55caf76d0d37aa6fdf2a5a672222995
Fredrik Jonsson <fredrik.siljeblad@gmail.com>
2022-10-05 10:23:12 +0200

    Starting to change TeamList.jsx to use db instead of mockdata
    William fixed it in his branch. Ignore this one.

--- src/helper_functions/loadTeams.js
+++ src/helper_functions/loadTeams.js
@@ -1,5 +1,7 @@
-const loadTeams = async (userId) => {
+export const loadTeams = async (userId) => {
   const response = await fetch("/api/team/for_user?user_id=" + userId);
   const data = await response.json();
   return data;
 };
+
+
--- src/team_components/TeamList.jsx
+++ src/team_components/TeamList.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CreateBtn from '../kb-components/CreateBtn'
 import TeamItem from '../team_components/TeamItem'
 import testData from '../testData/test-data.json'
 import AddTeamBtn from "../kb-components/AddTeamBtn";
-import loadTeams from "../helper_functions";
+import { loadTeams } from "../helper_functions/loadTeams";
+
 
 function TeamList() {
-  return (
+
+    const [teams, setTeams] = useState([])
+    let userId = localStorage.getItem('active-user-id');
+
+    useEffect(() => {
+        // declare the async data fetching function
+        const fetchData = async () => {
+            // get the data from the api
+            const data = await fetch('https://yourapi.com');
+            // convert data to json
+            const json = await data.json();
+            return json;
+        }
+    }
+
+    let load =  async () => {
+        let teamLoad = await loadTeams(userId);
+        setTeams(teamLoad);
+    }
+
+    load();
+    console.log("teams:");
+    console.log(teams);
+    console.log("testData:");
+    console.log(testData);
+
+    return (
     <div>
         <a >
                 <div className='flex justify-around'>
@@ -17,13 +44,10 @@ function TeamList() {
             </a>
 
             <div  className='flex flex-col justify-start items-center'>
-                {testData.map((team) => ( 
+                {testData.map((team) => (
                   <TeamItem teamId={team.id} key={team.id} teamName={team.teamName}/>
                 ))}
             </div>
-
-           
-       
         </div>
   )
 }
