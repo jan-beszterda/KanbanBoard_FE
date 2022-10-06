98ea53085c1afc7432703580d5077858df5e2392
William Le <91021939+Will7e@users.noreply.github.com>
2022-10-06 11:50:04 +0200

    Update Extra.jsx

--- src/page_partials/Extra.jsx
+++ src/page_partials/Extra.jsx
@@ -14,7 +14,7 @@ function Extra() {
         className="flex items-center px-4 py-2 mt-3 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700"
         href="#"
       >
-        <span className="mx-4 font-medium">Friends</span>
+        <span className="mx-4 font-medium">Invitations</span>
       </a>
 
       <a
