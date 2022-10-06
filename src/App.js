1c120ee338e725b027972e04247b299301e545f5
Jan Beszterda <jan.beszterda@gmail.com>
2022-10-06 13:57:52 +0200

    fixing board page route

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
           }
         />
         <Route
-          path="/boardpage/:teamid/:id"
+          path="/boardpage/:id"
           element={
             <Layout>
               {" "}
