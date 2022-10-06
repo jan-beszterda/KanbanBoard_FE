4387ed13b9603d3cc084131074e4fc651b7330c4
Jan Beszterda <jan.beszterda@gmail.com>
2022-10-06 13:54:09 +0200

    fixing more merge errors

--- src/team_components/TeamPage.jsx
+++ src/team_components/TeamPage.jsx
@@ -1,7 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-import CreateBtn from "../kb-components/CreateBtn";
 import AddBoardBtn from "../kb-components/AddBoardBtn";
 import BoardItem from "../board_components/BoardItem";
 import LeaveTeamBtn from "./LeaveTeamBtn";
@@ -12,7 +11,7 @@ import { useNavigate } from "react-router-dom";
 function TeamPage() {
   const [team, setTeam] = useState();
   const params = useParams();
-  const userId = localStorage.getItem('active-user-id');
+  const userId = localStorage.getItem("active-user-id");
   const teamId = params.id;
   const navigate = useNavigate();
   const toLayout = () => navigate("/profilepage", { replace: true });
@@ -25,57 +24,51 @@ function TeamPage() {
     load();
   }, [params.id]);
 
-      const handleSubmit=(e)=> {           
-
-        const leaveTeam = async (data = {}) => {
-          console.log('/api/team/'+ params.id + '/leave?user_id=' + userId);
-          console.log(teamId);
-            let response = await fetch('/api/team/'+ params.id + '/leave?user_id=' + userId, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            console.log(response);
-            
-            if (response.status === 200) {
-                console.log('Left team successfully');
-            }
-
-            //let result = await response.json();
-            //console.log(response);
-
-            //return result;
+  const handleSubmit = (e) => {
+    const leaveTeam = async (data = {}) => {
+      console.log("/api/team/" + params.id + "/leave?user_id=" + userId);
+      console.log(teamId);
+      let response = await fetch(
+        "/api/team/" + params.id + "/leave?user_id=" + userId,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
+      );
+
+      console.log(response);
+
+      if (response.status === 200) {
+        console.log("Left team successfully");
+      }
+
+      //let result = await response.json();
+      //console.log(response);
+
+      //return result;
+    };
 
     leaveTeam().then(() => {
-      
-      toLayout(); 
-      window.location.reload();       
+      toLayout();
+      window.location.reload();
     });
-
-}
-// End handleSubmit.
-    
+  };
+  // End handleSubmit.
 
   return (
     <div className="w-full">
       <div>
         {team && <h1 className=" pt-5 text-3xl">{team.teamName}</h1>}
 
-<<<<<<< HEAD
         <div className=" flex gap-4 py-10">
           <AddBoardBtn
             name={"Board name"}
             btnName={"Create board"}
             teamId={params.id}
           />
-=======
-        <div className=" flex gap-4 py-10">        
-          <AddBoardBtn name={"Board name"} btnName={"Create board"} teamId={params.id}/>
-          <LeaveTeamBtn handleSubmit={handleSubmit}/>
->>>>>>> dev
+          <LeaveTeamBtn handleSubmit={handleSubmit} />
         </div>
       </div>
 
