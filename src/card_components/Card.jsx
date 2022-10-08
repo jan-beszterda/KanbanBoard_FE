ae1030785fcc216a0dee59348e4a1d3d468e77c9
Jan Beszterda <jan.beszterda@gmail.com>
2022-10-08 16:45:08 +0200

    added stomp and comment saving/loading

--- src/card_components/Card.jsx
+++ src/card_components/Card.jsx
@@ -1,8 +1,14 @@
 import { useState, useEffect } from "react";
 import { loadCard } from "../helper_functions/loadCard";
+import { createStompClient } from "../helper_functions/createStompClient";
+import { createComment } from "../helper_functions/createComment";
 
 const Card = (props) => {
   const [card, setCard] = useState();
+  const [cardAuthor, setCardAuthor] = useState();
+  const [cardComments, setCardComments] = useState([]);
+  const [client, setClient] = useState({});
+  const [isToBeUpdated, setIsToBeUpdated] = useState(false);
   const [newComment, setNewComment] = useState({
     commentText: "",
   });
@@ -10,10 +16,58 @@ const Card = (props) => {
   useEffect(() => {
     const load = async () => {
       let card = await loadCard(props.cardId);
+      setCardAuthor(card.author);
+      setCardComments(card.comments);
+      card.author = null;
+      card.comments = null;
       setCard(card);
     };
     load();
   }, [props.cardId]);
+
+  useEffect(() => {
+    let stompClient = createStompClient("/topic/card/" + props.cardId, () =>
+      setIsToBeUpdated(true)
+    );
+    setClient(stompClient);
+    return () => {
+      stompClient.deactivate();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isToBeUpdated) {
+      const load = async () => {
+        let card = await loadCard(props.cardId);
+        setCardAuthor(card.author);
+        setCardComments(card.comments);
+        card.author = null;
+        card.comments = null;
+        setCard(JSON.parse(card));
+      };
+      load();
+    }
+  }, [isToBeUpdated]);
+
+  const addComment = async () => {
+    let response = await createComment(
+      newComment,
+      localStorage.getItem("active-user-id"),
+      props.cardId
+    );
+    if (response.status === 200) {
+      setNewComment({
+        commentText: "",
+      });
+      client.publish({
+        destination: "/app/card/" + props.cardId,
+      });
+    }
+  };
+
+  const handleCommentFieldChange = (e) => {
+    setNewComment({ ...newComment, [e.target.name]: e.target.value });
+  };
 
   return (
     <>
@@ -32,10 +86,10 @@ const Card = (props) => {
               </button>
             </div>
             <div className="flex items-start justify-between border-b border-solid border-slate-200 p-5 pt-0 pb-3">
-              {card && (
+              {cardAuthor && (
                 <p className="text-xs font-light tracking-wide italic">
-                  Created by: {card.author.firstName} {card.author.lastName} (
-                  {card.author.email})
+                  Created by: {cardAuthor.firstName} {cardAuthor.lastName} (
+                  {cardAuthor.email})
                 </p>
               )}
             </div>
@@ -64,27 +118,30 @@ const Card = (props) => {
                 name="commentText"
                 placeholder="Enter comment"
                 value={newComment.commentText}
-                onChange={(e) => setNewComment(e.target.value)}
+                onChange={(e) => handleCommentFieldChange(e)}
               />
               <button
                 className="basis-1/5 font-sans font-normal text-sm mx-2 bg-dark-grey py-2 mb-2 px-2 rounded-lg shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
-                onClick={() => {}}
+                onClick={addComment}
                 type="button"
               >
                 Add comment
               </button>
             </div>
             <div className="flex flex-col items-start justify-between p-5 pt-1">
-              {card && card.comments.length !== 0 ? (
-                card.comments.map((comment) => (
+              {cardComments && cardComments.length !== 0 ? (
+                cardComments.map((comment) => (
                   <div
                     key={comment.commentId}
-                    className="border-b border-solid border-slate-200"
+                    className="border-b border-solid border-slate-200 p-2"
                   >
-                    <p className="text-xs font-light tracking-wide italic">
-                      On {comment.publishedOn} {comment.user} wrote:
+                    <p className="text-xs font-light tracking-wide italic my-1">
+                      On {comment.publishedOn} {comment.user.firstName}{" "}
+                      {comment.user.lastName} ({comment.user.email}) wrote:
                     </p>
-                    <p className="text-sm font-normal">{comment.commentText}</p>
+                    <p className="text-sm font-normal my-1">
+                      {comment.commentText}
+                    </p>
                   </div>
                 ))
               ) : (
