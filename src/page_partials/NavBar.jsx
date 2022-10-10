b3a66aca048fd573dc7de60466379e7c91e47991
William Le <91021939+Will7e@users.noreply.github.com>
2022-10-11 00:55:35 +0200

    Update NavBar.jsx

--- src/page_partials/NavBar.jsx
+++ src/page_partials/NavBar.jsx
@@ -7,7 +7,7 @@ function NavBar({name}) {
   return (
     <div>
         <div className="   bg-white fixed h-16 pl-40 border-2 py-10 border-gray-100  w-full flex items-center justify-around pr-5 z-10 ">
-            <div className=" flex px-5 items-center">
+            <div className=" flex px-5 items-center ml-44">
                 
                 <input type="text" placeholder="Search for anything ..."
                 className=" bg-gray-100 border-0 rounded text-black px-10 py-2 pr-36
