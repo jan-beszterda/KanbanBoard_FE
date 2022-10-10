f01dd25742ca5a215fa9a39627b135aadb46e316
William Le <91021939+Will7e@users.noreply.github.com>
2022-10-10 14:57:49 +0200

    Update TeamItem.jsx

--- src/team_components/TeamItem.jsx
+++ src/team_components/TeamItem.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function TeamItem(props) {
+  const [color, setColor] = React.useState("");
+
 
   let colorArray = [{color: "border-red-500"}, {color: "border-yellow-500"}, {color: "border-green-500"}, {color: "border-blue-500"}, {color: "border-indigo-500"}, {color: "border-purple-500"}, {color: "border-pink-500"}];
-
-  
   let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)].color;
-  console.log(randomColor);
+
+  let fincalColor = color ? color : randomColor;
+
+  useEffect(() => {
+    setColor(fincalColor);
+  }, []);
+
+  useEffect(() => {
+    window.sessionStorage.setItem("teamColor", color);
+  }, [color]);
+  
   
   return (
     <Link
-      className= {`text-center  font-bold text-xs uppercase mx-5 mb-2 border-b-2 rounded-b p-2 ${randomColor} hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150`}
+      className= {`text-center  font-bold text-xs uppercase mx-5 mb-2 border-b-2 rounded-b p-2 ${color} hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150`}
       key={props.teamId}
       id={props.teamId}
       to={`/teampage/${props.teamId}`}
