4ea704a73c62e596667f06c48116500176a1a251
William Le <91021939+Will7e@users.noreply.github.com>
2022-10-11 17:55:05 +0200

    Update CardItem.jsx

--- src/kb-components/CardItem.jsx
+++ src/kb-components/CardItem.jsx
@@ -2,11 +2,11 @@ function CardItem(props) {
   return (
     <button
       key={props.cardId}
-      className="flex flex-col bg-white w-[220px] rounded-xl p-4 my-3 items-start"
+      className="flex flex-col bg-white w-[220px] rounded-xl p-4 my-3 hover:shadow-lg drop-shadow-md"
       type="button"
       onClick={props.onClick}
     >
-      <p className="text-l font-semibold">{props.cardTitle}</p>
+      <p className="text-l font-semibold self-center">{props.cardTitle}</p>
     </button>
   );
 }
