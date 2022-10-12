7848aaec865a04f58b514a9fa44e550083524746
William Le <91021939+Will7e@users.noreply.github.com>
2022-10-12 10:31:25 +0200

    revert

--- src/board_components/BoardPage.jsx
+++ src/board_components/BoardPage.jsx
@@ -4,10 +4,13 @@ import { useParams } from "react-router-dom";
 import { loadBoard } from "../helper_functions/loadBoard";
 import Column from "../kb-components/Column";
 import AddColBtn from "../kb-components/AddColBtn";
+import { createStompClient } from "../helper_functions/createStompClient";
+import UpdateBoardBtn from "../kb-components/UpdateBoardBtn";
 
 function BoardPage() {
   const [board, setBoard] = useState();
-
+  const [client, setClient] = useState({});
+  const [isToBeUpdated, setIsToBeUpdated] = useState(false);
   const params = useParams();
 
   useEffect(() => {
@@ -18,39 +21,38 @@ function BoardPage() {
     load();
   }, [params.id]);
 
+  useEffect(() => {
+    let stompClient = createStompClient("/topic/board/" + params.id, () =>
+      setIsToBeUpdated(true)
+    );
+    setClient(stompClient);
+    return () => {
+      stompClient.deactivate();
+    };
+  }, []);
 
-  const handleSubmit=(e)=> {
-
-  const createColumn = async (data = {}) => {
-    let response = await fetch("/api/column/create?board_id=" + params.id, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (response.status === 200) {
-      console.log("Column created successfully");
+  useEffect(() => {
+    if (isToBeUpdated) {
+      const load = async () => {
+        let board = await loadBoard(params.id);
+        setBoard(board);
+      };
+      load();
+      setIsToBeUpdated(false);
     }
+  }, [isToBeUpdated]);
 
-    //let result = await response.json();
-    //console.log(result);
 
-    //return result;
-  };
-
-  createColumn().then(() => {
-    console.log("Success Creating Column");
-    window.location.reload();
-  });
-} 
+  // Make sure board exists before rendering the BoardPage.
+  if (!board) return;
 
   return (
     <div>
       <div className="relative h-auto mb-10 mt-5 flex flex-col justify-start text-start gap-12 w-auto">
-        {board && <h2 className="text-3xl mt-5 mr-5">{board.boardName}</h2>}
+        <h2 className="text-3xl mt-5 mr-5">{board.boardName}</h2>
+        <UpdateBoardBtn board={board} setBoard={setBoard}/>
         <button className="text-start">Back</button>
+
       </div>
       <div className="flex flex-row justify-evenly gap-5 flex-nowrap">
         {board &&
@@ -61,13 +63,19 @@ function BoardPage() {
               columnId={column.columnId}
               columnTitle={column.columnTitle}
               cards={column.cardList}
+              stompClient={client}
+              columns={board.columnList}
             />
           ))}
-        <AddColBtn name={"Title"} btnName={"+ Add column"} handleSubmit={handleSubmit}></AddColBtn>
+        <AddColBtn
+          name={"Add new column"}
+          btnName={"+ Add column"}
+          boardId={params.id}
+          stompClient={client}
+        />
       </div>
     </div>
   );
 }
-
 
 export default BoardPage;
