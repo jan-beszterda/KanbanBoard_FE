1f754c75addd2f45e45a9b617f714bec193a9a24
William Le <91021939+Will7e@users.noreply.github.com>
2022-10-13 20:11:48 +0200

    resolve merge conflict

--- src/team_components/TeamPage.jsx
+++ src/team_components/TeamPage.jsx
@@ -1,21 +1,23 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { FaPencilAlt } from "react-icons/fa";
+import React from "react";
 
 import AddBoardBtn from "../kb-components/AddBoardBtn";
 import BoardItem from "../board_components/BoardItem";
 import LeaveTeamBtn from "./LeaveTeamBtn";
 import InviteUserBtn from "./InviteUserBtn";
-import CreateBtn from "../kb-components/EditTeamBbtn";
 
 import { loadTeam } from "../helper_functions/loadTeam";
 import { createStompClient } from "../helper_functions/createStompClient";
-import { editTeamName } from "../helper_functions/editTeams";
+import { editTeam } from "../helper_functions/editTeams";
+import EditTeamBtn from "../kb-components/EditTeamBtn";
 
 function TeamPage() {
   const [team, setTeam] = useState();
   const [client, setClient] = useState({});
   const [isToBeUpdated, setIsToBeUpdated] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const params = useParams();
   const userId = localStorage.getItem("active-user-id");
@@ -47,114 +49,101 @@ function TeamPage() {
         setTeam(team);
       };
       load();
+      setIsToBeUpdated(false);
     }
   }, [isToBeUpdated]);
 
-  const handleSubmit = (e) => {
-    const leaveTeam = async (data = {}) => {
-      console.log("/api/team/" + params.id + "/leave?user_id=" + userId);
-      //console.log(teamId);
+  const handleSubmit = () => {
+    const leaveTeam = async () => {
       let response = await fetch(
         "/api/team/" + params.id + "/leave?user_id=" + userId,
         {
           method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
         }
       );
-
-      console.log(response);
-
-      if (response.status === 200) {
-        console.log("Left team successfully");
-      }
-
-      //let result = await response.json();
-      //console.log(response);
-
-      //return result;
+      return response;
     };
-
-    leaveTeam().then(() => {
-      toLayout();
-      window.location.reload();
+    leaveTeam().then((response) => {
+      if (response.status === 200) {
+        client.publish({ destination: "/app/team/" + team.id });
+        client.publish({ destination: "/app/teamlist/" + userId });
+        toLayout();
+      }
     });
   };
-  // End handleSubmit.
 
-
-  // edit team name
-  const [teamName, setTeamName] = useState("");
-  const [showModal, setShowModal] = useState(false);
-
-  const closeModal = () => {
-    setShowModal(false);
-  };
-
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setTeamName(e.target.value);
+  const handleChange = (fieldName, fieldValue) => {
+    setTeam({ ...team, [fieldName]: fieldValue });
   };
 
   const edit = () => {
-    editTeamName(params.id, teamName)
-    setTeam({...team, teamName: teamName})
-      closeModal();
+    editTeam(team.id, team).then((response) => {
+      if (response.status === 200) {
+        team.teamMembers.map((member) => {
+          client.publish({ destination: "/app/teamlist/" + member.userId });
+        });
+        client.publish({ destination: "/app/team/" + team.id });
+        setShowModal(false);
+      }
+    });
   };
 
   return (
-    <div className="w-full">
+    <div className="w-full flex flex-col">
       <div className="flex flex-wrap p-2 mb-6 border-b">
-        {team && (
-          <h1 className="  flex gap-5 flex-row flex-auto basis-4/5 flex-grow flex-shrink-0 text-3xl p-2  normal-case">
-            {team.teamName ? team.teamName : <span>[Name not set]</span>}
-
-            <FaPencilAlt
-              className=" cursor-pointer mt-2"
-              color="#FF8E7F"
-              size={"17px"}
-              type="button"
-              onClick={() => setShowModal(true)}
-            ></FaPencilAlt>
+        <div className="flex flex-col flex-grow flex-shrink-0 basis-4/5">
+          <div className="flex items-center">
+            {team && (
+              <h1 className="capitalize text-3xl p-2 ">
+                {team.teamName ? team.teamName : "[Name not set]"}
+              </h1>
+            )}
+            {team && (
+              <FaPencilAlt
+                className="cursor-pointer mx-2 my-2"
+                color="#FF8E7F"
+                size={"20px"}
+                type="button"
+                onClick={() => setShowModal(true)}
+              />
+            )}
             {showModal ? (
-              <CreateBtn
-              btnTitle={"Edit Team Name"}
+              <EditTeamBtn
                 edit={edit}
-                closeModal={closeModal}
+                closeModal={() => setShowModal(false)}
                 btnType={"Edit"}
-                value={teamName}
+                values={team}
                 onChange={handleChange}
               />
             ) : null}
-          </h1>
-        )}
-        <div className="flex-auto basis-1/5 flex-grow-0 flex-shrink p-2">
-          <button className="mr-0"></button>
-          <InviteUserBtn
-            name={"Invite user"}
-            btnName={"Invite user"}
-            teamId={params.id}
-          />
-          <LeaveTeamBtn className="ml-0" handleSubmit={handleSubmit} />
+          </div>
+          {team && (
+            <p className="text-l mt-4 p-2">
+              {team.teamDescription
+                ? team.teamDescription
+                : "[Description not set]"}
+            </p>
+          )}
         </div>
-        {team && (
-          <p className="text-l mt-4 p-2">
-            {team.teamDescription ? (
-              team.teamDescription
-            ) : (
-              <span>[Description not set]</span>
-            )}
-          </p>
-        )}
+        <div className="flex flex-col jusdtify-ityems-center basis-1/5 flex-grow-0 flex-shrink p-2">
+          {team && (
+            <InviteUserBtn
+              name={"Invite user"}
+              btnName={"Invite user"}
+              teamId={team.id}
+              stompClient={client}
+            />
+          )}
+          <LeaveTeamBtn handleSubmit={handleSubmit} />
+        </div>
       </div>
       <div className="flex gap-0 mb-6 border-b">
         <div className="basis-1/2 gap-2 border-r">
-          <h3 className="text-xl font-bold border-b p-2  ">Members</h3>
+          <h3 className="text-xl font-bold border-b p-2">Members</h3>
           {team &&
             (team.teamMembers.length !== 0 ? (
               team.teamMembers.map((member) => (
-                <p className="mb-2 p-2 capitalize" key={member.userId}>
+                <p className="mb-2 p-2  capitalize" key={member.userId}>
                   {member.firstName} {member.lastName} ({member.email})
                 </p>
               ))
@@ -167,7 +156,7 @@ function TeamPage() {
           {team &&
             (team.invited.length !== 0 ? (
               team.invited.map((invitee) => (
-                <p className="mb-2 p-2 capitalize" key={invitee.userId}>
+                <p className="mb-2 p-2" key={invitee.userId}>
                   {invitee.firstName} {invitee.lastName} ({invitee.email})
                 </p>
               ))
@@ -176,7 +165,7 @@ function TeamPage() {
             ))}
         </div>
       </div>
-      <div className=" rounded-md bg-light-grey flex flex-col justify-evenly">
+      <div className=" rounded-md bg-light-grey flex flex-col mb-20 justify-evenly normal-case">
         {team &&
           team.boards.map((board) => (
             <BoardItem
@@ -186,11 +175,14 @@ function TeamPage() {
               boardDescription={board.boardDescription}
             />
           ))}
-        <AddBoardBtn
-          name={"Board name"}
-          btnName={"+ New board"}
-          teamId={params.id}
-        />
+        {team && (
+          <AddBoardBtn
+            name={"Add new board"}
+            btnName={"+ New board"}
+            teamId={team.id}
+            stompClient={client}
+          />
+        )}
       </div>
     </div>
   );
