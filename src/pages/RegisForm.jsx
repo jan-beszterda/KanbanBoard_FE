a51d88277cc148cd3ad89a4b369407b0e305b5da
Jan Beszterda <jan.beszterda@gmail.com>
2022-10-14 10:57:33 +0200

    deleted console.log statements

--- src/pages/RegisForm.jsx
+++ src/pages/RegisForm.jsx
@@ -1,110 +1,107 @@
-import React from 'react'
-import FormHeader from '../form_components/FormHeader'
-import Input from '../form_components/Input'
-import { signupFields } from '../constants/formFields'
-import FormFooter from '../form_components/FormFooter'
-import FormButton from '../form_components/FormButton'
-import { useState } from 'react'
-import {Navigate, useNavigate} from "react-router-dom";
+import React from "react";
+import FormHeader from "../form_components/FormHeader";
+import Input from "../form_components/Input";
+import { signupFields } from "../constants/formFields";
+import FormFooter from "../form_components/FormFooter";
+import FormButton from "../form_components/FormButton";
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
-const fields=signupFields;
-let fieldsState={};
-fields.forEach(field => fieldsState[field.id]='');
+const fields = signupFields;
+let fieldsState = {};
+fields.forEach((field) => (fieldsState[field.id] = ""));
 
-function RegisForm({logo}) {
-  const [signupState,setSignupState]=useState(fieldsState);
+function RegisForm({ logo }) {
+  const [signupState, setSignupState] = useState(fieldsState);
 
-    const navigate = useNavigate();
-    const toLayout = () => navigate("/profilepage", { replace: true });
+  const navigate = useNavigate();
+  const toLayout = () => navigate("/profilepage", { replace: true });
 
-    const handleChange=(e)=>setSignupState({...signupState,[e.target.id]:e.target.value});
+  const handleChange = (e) =>
+    setSignupState({ ...signupState, [e.target.id]: e.target.value });
 
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(signupState)
 
-      function passwordConfirmed() {
-          if (signupState.password !== signupState.confirm_password) {
-              // clear password & confirm_password fields and give message that they must match
-              signupState.password = "";
-              signupState.confirm_password = "";
-              return false;
-          }
-          return true;
+    function passwordConfirmed() {
+      if (signupState.password !== signupState.confirm_password) {
+        // clear password & confirm_password fields and give message that they must match
+        signupState.password = "";
+        signupState.confirm_password = "";
+        return false;
       }
+      return true;
+    }
 
-      if(passwordConfirmed()){
-          createAccount();
-      }
-
-  }
+    if (passwordConfirmed()) {
+      createAccount();
+    }
+  };
 
   //handle Signup API Integration here
-  const createAccount=()=>{
-      const user = {
-        "userId":"",
-        "firstName":signupState.firstName,
-        "lastName":signupState.lastName,
-        "password":signupState.password,
-        "email":signupState.email,
-        "teams":[],
-        "invitations":[]
-      }
+  const createAccount = () => {
+    const user = {
+      userId: "",
+      firstName: signupState.firstName,
+      lastName: signupState.lastName,
+      password: signupState.password,
+      email: signupState.email,
+      teams: [],
+      invitations: [],
+    };
 
-      const signUpUser = async (data={}) => {
-        let response = await fetch('/api/user', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'},
-          body: JSON.stringify(data)
-        });
+    const signUpUser = async (data = {}) => {
+      let response = await fetch("/api/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
       let result = await response.json();
 
-          if (response.status === 200) {
-              console.log('User created successfully');
-          }
-        return result;
-      }
-      signUpUser(user).then((result) => {
-        console.log("Success Creating User");
-        console.log(result);
+      return result;
+    };
+    signUpUser(user).then((result) => {
+      localStorage.setItem("active-user-id", result.userId);
 
-          //Set localStorage for user id
-          localStorage.setItem('active-user-id', result.userId);
-          let idTest = localStorage.getItem('active-user-id');
-          console.log(idTest);
-          toLayout();
-      });
+      toLayout();
+    });
+  };
 
-
-    }
-  
-  
   return (
     <div>
+      <form
+        className="max-w-[400px] w-full ml-36 bg-white p-4 "
+        onSubmit={handleSubmit}
+      >
+        <FormHeader
+          mt={"mt-44"}
+          linkUrl={"/"}
+          logo={logo}
+          title={"Sign up"}
+          subtitle={"Already a user?"}
+        />
 
-        <form className='max-w-[400px] w-full ml-36 bg-white p-4 ' onSubmit={handleSubmit}>
-                <FormHeader mt={"mt-44"} linkUrl={"/"} logo={logo} title = {"Sign up"} subtitle ={"Already a user?"}/>
+        {signupFields.map((field) => (
+          <Input
+            key={field.id}
+            id={field.id}
+            handleChange={handleChange}
+            value={signupState[field.id]}
+            placeholder={field.placeholder}
+            labelText={field.labelText}
+            type={field.type}
+            isRequired={field.isRequired}
+          ></Input>
+        ))}
+        <FormFooter />
 
-                {signupFields.map((field) => (
-                    <Input 
-                      key={field.id} 
-                      id={field.id}
-                      handleChange={handleChange}
-                      value={signupState[field.id]}
-                      placeholder={field.placeholder} 
-                      labelText={field.labelText} 
-                      type={field.type} 
-                      isRequired={field.isRequired}>
-                    </Input>
-                ))}
-                    <FormFooter />
-
-                <FormButton text={"Register"} onSubmit={handleSubmit}/>    
-            </form>
+        <FormButton text={"Register"} onSubmit={handleSubmit} />
+      </form>
     </div>
-  )
+  );
 }
 
-export default RegisForm
+export default RegisForm;
